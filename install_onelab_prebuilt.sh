990a148fa7d681071109c888fd6a2671f1004298
Benjamin Vial <b.vial@qmul.ac.uk>
2018-06-22 16:54:00 +0100

    travis test

--- install_onelab_prebuilt.sh
+++ install_onelab_prebuilt.sh
@@ -3,12 +3,18 @@
 export ONELAB_PATH=$TRAVIS_BUILD_DIR
 cd $ONELAB_PATH
 mkdir bin
+
+echo "INSTALLING GMSH"
+echo '----------------'
 # gmsh
 wget -c http://gmsh.info/bin/Linux/gmsh-git-Linux64.tgz
 tar -xvf gmsh-git-Linux64.tgz
 rm gmsh-git-Linux64.tgz
 mv gmsh-git-Linux64/bin/gmsh $ONELAB_PATH/bin
 rm -rf gmsh-git-Linux64
+
+echo "INSTALLING GETDP"
+echo '----------------'
 # getdp
 wget -c http://getdp.info/bin/Linux/getdp-git-Linux64c.tgz
 tar -xvf getdp-git-Linux64c.tgz
