451bcdf9ec5380ca8ebb4e210cf263fccdba5c2f
Benjamin Vial <b.vial@qmul.ac.uk>
2018-06-23 16:06:12 +0100

    travis

--- install_nlopt.sh
+++ install_nlopt.sh
@@ -2,6 +2,9 @@
 
 echo "INSTALLING NLOPT"
 echo '----------------'
+export INSTALL_PATH=$TRAVIS_BUILD_DIR/opt
+mkdir $INSTALL_PATH
+cd $INSTALL_PATH
 wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz
 tar zxvf nlopt-2.4.2.tar.gz
 rm nlopt-2.4.2.tar.gz
@@ -10,9 +13,6 @@ mv nlopt-2.4.2 nlopt
 cd nlopt
 make clean
 make distclean
-
-export INSTALL_PATH=$TRAVIS_BUILD_DIR/opt
-mkdir $INSTALL_PATH
 ./configure --prefix=$INSTALL_PATH --enable-shared \
             --without-guile --without-matlab --without-octave
 make
--- install_swig.sh
+++ install_swig.sh
@@ -2,11 +2,14 @@
 
 echo "INSTALLING SWIG"
 echo '----------------'
+export INSTALL_PATH=$TRAVIS_BUILD_DIR/opt
+mkdir $INSTALL_PATH
+cd $INSTALL_PATH
 git clone https://github.com/swig/swig.git
 cd swig
 ./autogen.sh
-./configure
+./configure --prefix=$INSTALL_PATH 
 make
-sudo make install
+make install
 cd ..
 rm -rf swig
