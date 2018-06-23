25951fe0c4fd2fb15ecca6a4042137246b834e6d
Benjamin Vial <b.vial@qmul.ac.uk>
2018-06-23 15:30:26 +0100

    travis

--- install_nlopt.sh
+++ install_nlopt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+export TRAVIS_BUILD_DIR=$PWD
 echo "INSTALLING NLOPT"
 echo '----------------'
 wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz
@@ -10,6 +10,7 @@ mv nlopt-2.4.2 nlopt
 cd nlopt
 make clean
 make distclean
+
 export INSTALL_PATH=$TRAVIS_BUILD_DIR
 ./configure --prefix=$INSTALL_PATH --enable-shared \
             --without-guile --without-matlab --without-octave
