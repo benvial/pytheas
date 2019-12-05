a06af7829ada66d48c2d1c7b5b466ecd38858c05
Benjamin Vial <b.vial@qmul.ac.uk>
2019-12-05 16:54:38 +0000

    test docs

--- .ci/post.sh
+++ .ci/post.sh
@@ -1,66 +1,52 @@
 #!/bin/bash
 
-
+# Build only on Linux
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   source activate testenv
   set -e
-
-  
-
-
   if [  -d $HOME/.texlive  ]; then
-    echo "cached texlive found -- nothing to do";
+    echo "cached Texlive found -- nothing to do";
     CACHED_TEX=1;
   else
-    echo "cached texlive not found";
+    echo "cached Texlive not found";
   fi
 
 
-cd docs
-make clean
-make less
+  cd docs
+  make clean
+  make less
 
-unset CACHED_TEX
-  ####
-if [ "$CACHED_TEX" ]; then
-    echo ">>> Using cached tex environment";
+  unset CACHED_TEX
+
+  if [ "$CACHED_TEX" ]; then
+      echo ">>> Using cached Tex environment";
+    else
+      echo ">>> Building Tex environment";
+      cd ../.ci
+      source ./texlive/texlive_install.sh
+      cd ../docs
+      make latex
+      cd ./_build/latex/
+      # Texliveonfly will download missing packages automatically
+      texliveonfly -c xelatex pytheas.tex
+      cd ../..
+    fi
 
     make latexpdf
+    make html
+    # Copy pdf doc to _downloads
+    mv ./_build/latex/pytheas.pdf ./_build/html/_downloads/pytheas.pdf
+    # Copy apt requirements doc to binder directory
+    cp ./binder/apt.txt ./_build/html/binder
 
-  else
-    echo ">>> Building tex environment";
-    cd ../.ci
-    source ./texlive/texlive_install.sh
-    cd ../docs
-    make latex
-    cd ./_build/latex/
-    # Texliveonfly will download missing packages automatically
-    texliveonfly -c xelatex pytheas.tex
-    cd ../..
+    # List Tex packages
+    export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+    tlmgr list --only-installed | grep -oP 'i \K.+?(?=:)'
 
-  fi
-
-  make html
-
-  mv ./_build/latex/pytheas.pdf ./_build/html/_downloads/pytheas.pdf
-
-
-  cp ./binder/apt.txt ./_build/html/binder
-
-
-  ## tectonic
-  # tectonic --version
-  # tectonic ./_build/latex/pytheas.tex
-
-
-  export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
-  tlmgr list --only-installed | grep -oP 'i \K.+?(?=:)'
-
-  cd ..
-  doctr deploy . --built-docs docs/_build/html
-
-  codecov
-  export CODACY_PROJECT_TOKEN=50a1d9ea26e241f7bfc27117f783e4c0
-  coverage xml
-  python-codacy-coverage -r coverage.xml
+    cd ..
+    doctr deploy . --built-docs docs/_build/html
+    codecov
+    export CODACY_PROJECT_TOKEN=50a1d9ea26e241f7bfc27117f783e4c0
+    coverage xml
+    python-codacy-coverage -r coverage.xml
 fi
